e401bb6045bfd14d27f94201e06ea153758ac4e4
g922 <gk3446724@gmail.com>
2024-10-24 20:05:14 +0530

    string

--- 01_BASICS/06_String.js
+++ 01_BASICS/06_String.js
@@ -1,9 +1,29 @@
-const name = "gourav"
-const repo = 10
+const name = "gourav";
+const repo = 10;
 
-// normal way to concatenate the string 
-console.log(name + repo + "value");
+// normal way to concatenate the string
+console.log(name + repo + "value"); //gourav10value
 
 // now modern way to con catenate the string we use backstick as string interpolation which uses ${} pockets to add string
 
 console.log(`hello my name is ${name} and my repo count is ${repo}`);
+//hello my name is gourav and my repo count is 10
+//
+
+//constructer way to declare the string
+const nameGame = new String("assassins creed");
+// string is object of javascript which give the intendeation to its element
+// if we se in nameGame here a[0],s[1],s[2],..............d[14]
+
+console.log(nameGame[0]); //a
+console.log(nameGame.__proto__); //{} show the prototype
+
+//there are various functions which can be used to modify the string
+//ex
+console.log(nameGame.length); //15
+console.log(nameGame.toUpperCase); // ASSASSINS CREED
+//charAt
+//subString
+//indexof
+//
+//for more methods of string visit "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String"
